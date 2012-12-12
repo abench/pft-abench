6d317ba6a3f61384b3cfa2a06a7a1338e94fd1f6
andriy.bench <andriy.bench@gmail.com>
2012-12-12 13:51:31 +0200

    GroupRemovalTests were rewritten.

--- addressbook-selenium-tests/src/com/example/tests/GroupRemovalTests.java
+++ addressbook-selenium-tests/src/com/example/tests/GroupRemovalTests.java
@@ -1,5 +1,10 @@
 package com.example.tests;
 
+import static org.testng.AssertJUnit.assertEquals;
+
+import java.util.Collections;
+import java.util.List;
+
 import org.testng.annotations.Test;
 
 public class GroupRemovalTests extends TestBase{
@@ -7,10 +12,27 @@ public class GroupRemovalTests extends TestBase{
 	
 	@Test
 	public void deleteSomeGroup(){
+		
 		app.getNavigationHelper().openMainPage();
 		app.getNavigationHelper().gotoGroupPage();
+		
+		// save old state
+		List<GroupData> oldList = app.getGroupHelper().getGroups();
+
+		// execute test
 		app.getGroupHelper().deleteGroup(1);
-		app.getNavigationHelper().returnToGroupPage();		
+		app.getNavigationHelper().returnToGroupPage();
+		// save new state
+		List<GroupData> newList = app.getGroupHelper().getGroups();
+		// compare quantity
+		assertEquals(oldList.size(),newList.size()+1);
+		// Compare content
+		
+		oldList.remove(0);		
+		Collections.sort(oldList);
+		Collections.sort(newList);
+		assertEquals(oldList, newList);	
+
 	}
 
 }
