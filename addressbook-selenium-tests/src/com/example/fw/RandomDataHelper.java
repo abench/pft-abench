ba395363f05db7a912c095fe76e6e6d2c0f61fd4
andriy.bench <andriy.bench@gmail.com>
2013-01-05 22:45:31 +0200

    Fixed error at generateRandomString method (negative random string length). GroupDataGenerator is working properly now.

--- addressbook-selenium-tests/src/com/example/fw/RandomDataHelper.java
+++ addressbook-selenium-tests/src/com/example/fw/RandomDataHelper.java
@@ -65,9 +65,10 @@ public class RandomDataHelper{
 				"Q","W","E","R","T","Y","U","I","O","P","A","S","D","F","G","H",
 				"J","K","L","Z","X","C","V","B","N","M"};
 		String str = "";
+		int stringBound = 200;
 		Random rnd = new Random();
 		if (rnd.nextInt(10)!=0){
-			int stringLength = rnd.nextInt();
+			int stringLength = rnd.nextInt(stringBound);
 			for (int i=0; i<stringLength;i++){
 				str=str+alphavitArray[rnd.nextInt(alphavitArray.length)];
 			}			
