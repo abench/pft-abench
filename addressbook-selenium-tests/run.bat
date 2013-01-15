90d8da83989c851a1e92f5f68189caa7faed2f2f
andriy.bench <andriy.bench@gmail.com>
2013-01-15 17:42:48 +0200

    Added variables to run.bat.

--- addressbook-selenium-tests/run.bat
+++ addressbook-selenium-tests/run.bat
@@ -1,6 +1,9 @@
 del groups.txt
 del groups.xml
-java -cp bin com.example.tests.GroupDataGenerator 5 groups.txt csv
-java -cp bin;c:\javalibs\selenium\selenium-server-standalone-2.26.0.jar;c:\javalibs\xstream\xstream-1.4.3\lib\xstream-1.4.3.jar;c:\javalibs\xstream\xstream-1.4.3\lib\xstream\xpp3_min-1.1.4c.jar;c:\javalibs\xstream\xstream-1.4.3\lib\xstream\xmlpull-1.1.3.1.jar com.example.tests.GroupDataGenerator 5 groups.xml xml
-java -cp bin;c:\javalibs\selenium\selenium-server-standalone-2.26.0.jar;c:\javalibs\xstream\xstream-1.4.3\lib\xstream-1.4.3.jar;c:\javalibs\xstream\xstream-1.4.3\lib\xstream\xpp3_min-1.1.4c.jar;c:\javalibs\xstream\xstream-1.4.3\lib\xstream\xmlpull-1.1.3.1.jar -DconfigFile=firefox.properties org.testng.TestNG testng-customsuite.xml
- 
+
+SET TESTLIB=bin;c:\javalibs\selenium\selenium-server-standalone-2.26.0.jar;c:\javalibs\xstream\xstream-1.4.3\lib\xstream-1.4.3.jar;c:\javalibs\xstream\xstream-1.4.3\lib\xstream\xpp3_min-1.1.4c.jar;c:\javalibs\xstream\xstream-1.4.3\lib\xstream\xmlpull-1.1.3.1.jar
+SET CONFIG_FILE=firefox.properties
+
+java -cp %TESTLIB% com.example.tests.GroupDataGenerator 5 groups.txt csv
+java -cp %TESTLIB% com.example.tests.GroupDataGenerator 5 groups.xml xml
+java -cp %TESTLIB% -DconfigFile=%CONFIG_FILE% org.testng.TestNG testng-customsuite.xml
